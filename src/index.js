d2195354be743c585268e39178290347f65fa75e
sarvusahitya <sarvusahitya@gmail.com>
2025-01-04 23:03:15 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const { PORT } = require('./config');
-const { databaseConnection } = require('./database');
-const expressApp = require('./express-app');
-const errorHandler = require('./utils/errors');
+const express = require("express");
+const { PORT } = require("./config");
+const { databaseConnection } = require("./database");
+const expressApp = require("./express-app");
+const errorHandler = require("./utils/errors");
 
 const StartServer = async () => {
   const app = express();
-  const cors = require('cors');
-  app.use(cors({
-    origin: '*',
-    methods: '*',
-    allowedHeaders: '*'
-  }));
-  
+  const cors = require("cors");
+  app.use(
+    cors({
+      origin: "*",
+      methods: "*",
+      allowedHeaders: "*",
+    })
+  );
+
   await databaseConnection();
 
-  app.get('/', (req, res) => res.send('Welcome To Sarvu Sahitya'));
+  app.get("/", (req, res) => res.send("Welcome To Sarvu Sahitya"));
+
+  const routes = require("./routes");
 
   await expressApp(app);
   errorHandler(app);
@@ -23,11 +27,11 @@ const StartServer = async () => {
     .listen(PORT, () => {
       console.log(`listening to port ${PORT}`);
     })
-    .on('error', (err) => {
+    .on("error", (err) => {
       console.log(err);
       process.exit();
     })
-    .on('close', () => {
+    .on("close", () => {
       channel.close();
     });
 };
